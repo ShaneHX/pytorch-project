48f011cb9b31cf1f09eeee377e7e53ff30b11c3d
hanxin <hanxin1@geely.com>
2019-08-23 14:13:15 +0800

    Add TODO - Integration 'data_prefetcher' to BaseDataLoader
    'data_prefetcher' is a method acculate data load before training in APEX which is proposed by NVIDIA

--- base/base_data_loader.py
+++ base/base_data_loader.py
@@ -1,14 +1,55 @@
+#!/usr/bin/env python3
+# coding=UTF-8
+'''
+@Author: Shane
+@since: 2019-08-22 10:06:48
+@lastTime: 2019-08-23 10:27:58
+@LastAuthor: Shane
+@Description: A base class for all data loaders
+'''
+'''
+TODO: 
+[] Integration `data_prefetcher`, a method used in apex for speed up dataload
+[Source code](https://github.com/NVIDIA/apex/blob/f5cd5ae937f168c763985f627bbf850648ea5f3f/examples/imagenet/main_amp.py#L256)
+[Insid principle](https://github.com/NVIDIA/apex/issues/304)
+'''
+
 import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
 from torch.utils.data.sampler import SubsetRandomSampler
+from loguru import logger
 
 
 class BaseDataLoader(DataLoader):
-    """
+    """[summary]
     Base class for all data loaders
     """
     def __init__(self, dataset, batch_size, shuffle, validation_split, num_workers, collate_fn=default_collate):
+        """[summary]
+        
+        Parameters
+        ----------
+        DataLoader : [type]
+            [description]
+        dataset : [type]
+            [description]
+        batch_size : [type]
+            [description]
+        shuffle : [type]
+            [description]
+        validation_split : [type]
+            [description]
+        num_workers : [type]
+            [description]
+        collate_fn : [type], optional
+            [description], by default default_collate
+        
+        Returns
+        -------
+        [type]
+            [description]
+        """
         self.validation_split = validation_split
         self.shuffle = shuffle
 
@@ -27,7 +68,19 @@ class BaseDataLoader(DataLoader):
         super().__init__(sampler=self.sampler, **self.init_kwargs)
 
     def _split_sampler(self, split):
-        if split == 0.0:
+        """[summary]
+            Generate the validation and train sampler for Dataloader
+        Parameters
+        ----------
+        split : [type]: double or float
+            [description]: The percentage of the validation set of the dataset
+        
+        Returns
+        -------
+        [type]
+            [description]
+        """
+        if(split > 0.0) and (split < 1.0):
             return None, None
 
         idx_full = np.arange(self.n_samples)
