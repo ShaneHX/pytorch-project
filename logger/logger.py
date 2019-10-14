4e90c2e25a39fa89870c37c31a85e6e4ac1cea4b
hanxin <hanxin1@geely.com>
2019-10-14 14:45:58 +0800

    Add a logger base of  the package of loguru

--- logger/logger.py
+++ logger/logger.py
@@ -2,13 +2,27 @@ import os
 from loguru import logger
 
 
-def setup_logging(save_dir, model_name, is_training=True):
+def setup_logging(save_dir, model_name, timestamp, is_training=True):
+    """[summary]
 
+    Parameters
+    ----------
+    save_dir : [pathlib.PosixPath]
+        [description]: the absolute of the directory to save log file
+    model_name : [string]
+        [description]
+    timestamp : [string]
+        [description]
+    is_training : bool, optional
+        [description], by default True
+    """
     log_file_name = str(model_name)
     if is_training:
-        log_file_name = model_name + "_train_{time}.log"
+        log_file_name = "Train_" + \
+            str(model_name)+"_" + str(timestamp) + ".log"
     else:
-        log_file_name = model_name + "_test_{time}.log"
+        log_file_name = "Test_" + \
+            str(model_name) + "_" + str(timestamp)+".log"
 
-    log_file_name = os.path.join(os.path.abspath(save_dir), log_file_name)
-    logger.add(log_file_name)
+    log_file_name = save_dir.joinpath(log_file_name)
+    logger.add(str(log_file_name))
