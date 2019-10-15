d0a3db2df3e4dec2c1587a9140ff3245e95049a9
hanxin <hanxin1@geely.com>
2019-10-15 15:20:56 +0800

    Delete the try.py

--- try.py
+++
@@ -1,7 +0,0 @@
-
-from loguru import logger
-import multiprocessing
-multiprocessing.set_start_method('spawn', True)
-
-logger.warning("Warning: The number of GPU\'s configured to use is {}, but only {} are available "
-               "on this machine.".format(5, 2))
