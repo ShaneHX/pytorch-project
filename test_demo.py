693c02e930c21db7ad34beed8dc982e5a62e79ed
hanxin <hanxin1@geely.com>
2019-10-15 16:12:19 +0800

    Update the mode of test code

--- test_demo.py
+++ test_demo.py
@@ -1,24 +1,22 @@
 import argparse
 import torch
 from tqdm import tqdm
-import data_loader.data_loaders as module_data
+from data_loaders import CifarDataLoader
 import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
+from loguru import logger
 
 
 def main(config):
-    logger = config.get_logger('test')
 
     # setup data_loader instances
-    data_loader = getattr(module_data, config['data_loader']['type'])(
-        config['data_loader']['args']['data_dir'],
+    data_loader = CifarDataLoader(
+        data_dir="/home/shanehan/workspace/project_ws/pytorch-project/data/train.txt",
         batch_size=512,
         shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2
+        validation_split=0.0
     )
 
     # build model architecture
@@ -71,10 +69,12 @@ def main(config):
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
 
-    args.add_argument('-r', '--resume', default=None, type=str,
+    args.add_argument('-r', '--resume', default="/home/shanehan/workspace/project_ws/pytorch-project/saved/models/Demo/20191015-151912/checkpoint-epoch100.pth", type=str,
                       help='path to latest checkpoint (default: None)')
     args.add_argument('-d', '--device', default=None, type=str,
                       help='indices of GPUs to enable (default: all)')
+    args.add_argument('-t', '--training', default=True, type=bool,
+                      help='training or testing(default: training)')
 
     config = ConfigParser(args)
     main(config)
