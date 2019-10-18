882abfb77017eaf8b57dfac36cc32402a31b4b11
hanxin <hanxin1@geely.com>
2019-10-18 15:08:57 +0800

    Fix a bug about the saved dir not exsist

--- parse_config.py
+++ parse_config.py
@@ -31,7 +31,10 @@ class ConfigParser:
         self._config = _update_config(config, options, args)
 
         # set save_dir where trained model and log will be saved.
-        save_dir = Path(self.config['trainer']['save_dir']).resolve()
+        save_dir = Path(self.config['trainer']['save_dir'])
+        if not save_dir.exists():
+            save_dir.mkdir(exist_ok=True, parents=True)
+        save_dir = save_dir.resolve()
         timestamp = datetime.now().strftime(
             r'%Y%m%d-%H%M%S') if timestamp else ''
 
